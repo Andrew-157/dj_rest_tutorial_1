9e892ed1f72a5456e39f17b62d7259fd593396a4
Andrew Subotin <subotinandrey5@gmail.com>
2023-04-12 10:11:40 +0300

    Created Viewset For Language Model

--- rest_api/views.py
+++ rest_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Language
+from .serializers import LanguageSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class LanguageViewSet(viewsets.ModelViewSet):
+    serializer_class = LanguageSerializer
+    queryset = Language.objects.all()
